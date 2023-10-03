c50dcbab78d1844f235769a6265919aee854cdc0
serrudojc <serrudo.jc@gmail.com>
2023-10-02 21:21:32 -0300

    nuevo commit que se tiene que ejecutar en Jenkins

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Primer commit");
         System.out.println("TE odio JENKINS!!!!");
+        System.out.println("Prueba nuevo commit. Se ejecuta en Jenkins?");
     }
 }
